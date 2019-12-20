747c47d9792ad9f922846dc3f661f7737d4ecc68
Fei Q <feiwqiu@gmail.com>
2019-12-19 21:06:26 -0500

    Final_fq2145

--- xllblack/xll_gamma.cpp
+++ xllblack/xll_gamma.cpp
@@ -20,3 +20,20 @@ test test_gamma([] {
 });
 
 //!!! Implement XLL.GAMMA.PUT
+
+AddIn xai_gamma_put(
+	Function(XLL_DOUBLE, L"?xll_gamma_put", CATEGORY L".GAMMA.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the spot price. ")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility. ")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike. ")
+	.Arg(XLL_DOUBLE, L"t", L"is the time in years to maturity. ")
+	.Category(L"XLL")
+	.FunctionHelp(L"Return the value of a put option.")
+	.Documentation(L"Return the value of a put option." )
+);
+double WINAPI xll_gamma_put(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+
+	return gamma::put(f, sigma, k, t);
+}
