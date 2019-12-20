33eb8874da050012456b2b83445d2f070ba2e423
YutingLiu-yl4419 <yl4419@columbia.edu>
2019-12-19 21:23:28 -0500

    yl4419

--- xllblack/fms_gamma.h
+++ xllblack/fms_gamma.h
@@ -25,7 +25,7 @@ namespace fms::gamma {
 
 	inline double cdf(double x, double a, double b)
 	{
-		return ::igam(a, b * x) / ::gamma(a);
+		return ::igam(a, b * x); // ::gamma(a);
 	}
 
 	// The Gamma distribution has density function g(x) = x ^ (a - 1) exp(-b x) b ^ a / Gamma(a), x > 0,
@@ -34,12 +34,15 @@ namespace fms::gamma {
 	// The Black distribution is F = f exp(s Z - s^2/2), where Z is standard normal and s = sigma sqrt(t).  
 	// It has mean f and variance f^2 (exp(s^2) - 1).
 	//
-	// We Gamma distribution has F = f G, where G has mean 0 and variance exp(s^2) - 1
+	// We Gamma distribution has F = f G, where G has mean 1 and variance exp(s^2) - 1
 	// Solving 1 = a/b and (exp(s^2) - 1) = a/b^2 gives
 	// a = b and b = 1/(exp(s^2) - 1).
 	inline std::pair<double, double> convert(double s)
 	{
 		//!!! return (a, b) above
+		s = 1 / (exp(s * s) - 1);
+		
+
 		return std::pair(s, s);
 	}
 
@@ -48,12 +51,10 @@ namespace fms::gamma {
 	inline double put(double f, double sigma, double k, double t)
 	{
 		double s = sigma * sqrt(t);
+		double a = convert(s).first;
+		double b = convert(s).second;
+		return k * gamma::cdf(k/f, a , b) - f*gamma::cdf(k/f, a + 1, b);
 
-		//!!! delete this comment and the next three lines
-		s = s;
-		f = f;
-		k = k;
-		//!!! calculate put value
-		return 0;
+
 	}
 }
--- xllblack/xll_gamma.cpp
+++ xllblack/xll_gamma.cpp
@@ -20,3 +20,31 @@ test test_gamma([] {
 });
 
 //!!! Implement XLL.GAMMA.PUT
+static AddIn xai_gamma_put(
+	Function(XLL_DOUBLE, L"?xll_gamma_put", L"XLL.GAMMA.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the forward.", L"100")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility.", L"0.2")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike.", L"100")
+	.Arg(XLL_DOUBLE, L"t", L"is the time in years to expiration.", L"0.25")
+	.Category(L"XLL")
+	.FunctionHelp(L"Return the Gamma put value.")
+	.Documentation(
+		L"The value of a Gamma put option is E[(k - F)^+]. "
+		L"The expected value is k P(F <= k) - E[F 1(F <= k)], "
+		L"E[F 1(G <= k/f)] = f int_0^k x g(x) dx = f gamma::cdf(k/f, a + 1, b). "
+	)
+);
+double WINAPI xll_gamma_put(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	double result = std::numeric_limits<double>::quiet_NaN();
+
+	try {
+		result = fms::gamma::put(f, sigma, k, t);
+	}
+	catch (const std::exception & ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return result;
+}
