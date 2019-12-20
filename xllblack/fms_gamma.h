d5eccaf31fc07dc47ec62e04f888a19fea9ff9db
miaow1 <mw3302@columbia.edu>
2019-12-19 22:46:14 -0500

    mw3302

--- xllblack/fms_gamma.h
+++ xllblack/fms_gamma.h
@@ -2,7 +2,7 @@
 #pragma once
 #include <cmath>
 #include <tuple>
-#include "../xll12/xll/ensure.h"
+
 //
 // Cephes library declarations
 //
@@ -18,17 +18,14 @@ extern "C" double igam(double a, double x);
 namespace fms::gamma {
 
 	// Gamma distribution: g(x) = x^{a-1} exp(-b x) b^a/Gamma(a), x > 0
-	//b=1/beta
 	inline double pdf(double x, double a, double b)
 	{
-		if (x <= 0) return 0;
 		return pow(x, a - 1) * exp(-b * x) * pow(b, a) / ::gamma(a);
 	}
 
 	inline double cdf(double x, double a, double b)
 	{
-		if (x <= 0) return 0;
-		return ::igam(a, b * x); 	
+		return ::igam(a, b * x);
 	}
 
 	// The Gamma distribution has density function g(x) = x ^ (a - 1) exp(-b x) b ^ a / Gamma(a), x > 0,
@@ -37,37 +34,25 @@ namespace fms::gamma {
 	// The Black distribution is F = f exp(s Z - s^2/2), where Z is standard normal and s = sigma sqrt(t).  
 	// It has mean f and variance f^2 (exp(s^2) - 1).
 	//
-	// We Gamma distribution has F = f G, where G has mean 1 and variance exp(s^2) - 1
+	// We Gamma distribution has F = f G, where G has mean 0 and variance exp(s^2) - 1
 	// Solving 1 = a/b and (exp(s^2) - 1) = a/b^2 gives
 	// a = b and b = 1/(exp(s^2) - 1).
 	inline std::pair<double, double> convert(double s)
 	{
 		//!!! return (a, b) above
-		
-
-		double a = 1 / (exp(s * s) - 1);
-		return std::pair(a, a);//
-
-
-		
+		s = 1 / (exp(s * s) - 1);
+		return std::pair(s, s);
 	}
 
 	// Put value is E[(k - F)^+] = k P(F <= k) - E[F 1(F <= k)]
 	// E[F 1(G <= k/f)] = f int_0^k x g(x) dx = f gamma::cdf(k/f, a + 1, b)
 	inline double put(double f, double sigma, double k, double t)
 	{
-		
-		double a;
-		double b;//
 		double s = sigma * sqrt(t);
-		std::pair<double, double> ab = convert(s);
-		a = ab.first;
-		b = ab.second;
-		double put;
-		//
-		put = k * gamma::cdf(k / f, a, b) - f * gamma::cdf(k / f, a + 1, b);
-		//!!! calculate put value
-		return put;
 
+		
+		//!!! calculate put value
+		double a = gamma::convert(s).first;
+		return k * gamma::cdf(k / f, b, b) - f * gamma::cdf(k / f, b + 1, b);
 	}
 }
